225bd77a93dbd354253efb361017920c6e3939b3
Kateryna Doroshenko <kateryna.doroshenko@eleks.com>
2018-08-21 08:51:46 +0300

    base

---
+++ PageBase.js
@@ -0,0 +1,21 @@
+class PageBase {
+    constructor(login, password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    displayInfo() {
+        console.log(`Login: ${this.logine};
+        Password: ${this.password};
+    }
+}
+
+
+
+
+
+const PageBase = {
+
+}
+
+export
