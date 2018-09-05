64192c447fab58fd55086e74e4e9446505f48517
Kateryna Doroshenko <kateryna.doroshenko@eleks.com>
2018-09-05 17:44:35 +0300

    imveing

---
+++ Pageobject/pageobject.js
@@ -0,0 +1,38 @@
+const LOCATORS = {
+    myKanbanPane: ".my-kanban",
+    brainstormSection: ".task-titles.queue",
+    brainstormItemTitleElement: ".task-tiles.queue .task-tile .title"
+
+};
+
+export default class KanbanBoardPage extends NormalPage {
+
+    constructor(){
+        super();
+        this.waitForPageAvailable();
+        }
+
+    waitForPageAvailable() {
+        this._getMyKanbanPane().isVisible();
+        this._getBrainstormSection().isVisible();
+    }
+
+    isItemPresentUnderBrainstormSection(itemname){
+        for (let elem of this._getBrainstormItemTitleElement().findAll()){
+            if((elem.getText() ===itemName) && elem.isVisible())
+                return true;
+        }
+        return false;
+    }
+
+    /*
+    *Page element getters
+    */
+
+    _getMyKanbanPane(force = false){
+        if(!force && this.myKanbanPane)
+            return this.myKanbanPane;
+        this.myKanbanPane = new TextView(this, $(LOCATORS.myKanbanPane), "Kanban pane");
+        return this.myKanbanPane;
+    }
+}
